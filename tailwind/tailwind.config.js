265aa50ba40114ae953e57dc2cb17e71b2927a26
ptrichr <ho.n8te@gmail.com>
2024-04-24 02:46:06 -0400

    missed some stuff

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./flask_app/templates/*.{html,js,jsx,ts,tsx,vue,astro,svelte}'],
+  content: ['../flask_app/templates/*.{html,js,jsx,ts,tsx,vue,astro,svelte}'],
   theme: {
     extend: {},
   },
