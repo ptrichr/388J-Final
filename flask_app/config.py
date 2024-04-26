265869b0ac3c9751b15e8ecddfaff343dcf1b231
ptrichr <ho.n8te@gmail.com>
2024-04-26 14:39:01 -0400

    untracked file

--- flask_app/config.py
+++
@@ -1,2 +0,0 @@
-SECRET_KEY = b';\xe1\xce\x00A\x8e_g\x88I"\xab\'\xce\x06\xb9'
-MONGODB_HOST = 'mongodb+srv://petrichor:QpssdcS6BuHsIKsV@388j.lvoyrb1.mongodb.net/388J-Final?retryWrites=true&w=majority'
