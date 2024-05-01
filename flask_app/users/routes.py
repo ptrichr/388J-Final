287bbd4995478d36d5829ccc311cac759be7d9e7
Danko0o0o <arjungmoney@gmail.com>
2024-04-30 23:07:43 -0400

    base, login, register templates

--- flask_app/users/routes.py
+++ flask_app/users/routes.py
@@ -26,7 +26,6 @@ def register():
     if form.validate_on_submit():
         hashed = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, 
-                    email=form.email.data, 
                     password=hashed)
         user.save()
         
