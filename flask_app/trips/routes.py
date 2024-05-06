59c9661c54c8f4e84fbeddbd408fd10c5449d445
ptrichr <ho.n8te@gmail.com>
2024-05-05 20:10:39 -0400

    am i stupid?

--- flask_app/trips/routes.py
+++ flask_app/trips/routes.py
@@ -62,6 +62,7 @@ def index():
 def plan_trip(trip_title):
     form = POIForm()
     trip = Trip.objects(title=trip_title, author=current_user._get_current_object()).first()
+    
     if trip is not None:
         pois = list(trip.pois)
         # routes is a list of dictionaries that each contain a key "route" that is mapped
@@ -69,58 +70,59 @@ def plan_trip(trip_title):
         # dictionaries themselves
         routes = list(trip.routes)
         trip_info = zip(pois, routes)
+        
+        if request.method == "POST":
+            
+            if form.validate_on_submit():
+                poi_to_add = form.poi.data
+                leave_poi_t = form.depart.data
+                depart_cp = trip.start_time
+                
+                # formatting time as datetime object
+                departure_datetime = datetime(depart_cp.year,
+                                            depart_cp.month,
+                                            depart_cp.day,
+                                            leave_poi_t.hour,
+                                            leave_poi_t.minute)
+                
+                # logic for if we need to compute route from college park (adding first POI)
+                if not pois:
+                    route_info = client.compute_route("University of Maryland, College Park", 
+                                                    poi_to_add, 
+                                                    depart_cp)
+                    
+                    if route_info is None:
+                        flash(message="There was an error in route computation, please try again")
+                    else:
+                        trip.pois.append({
+                                            'name': poi_to_add,
+                                            "departure": departure_datetime
+                                        })
+                        trip.routes.append({'route': route_info})
+                        pprint(route_info)
+                        trip.save()
+                
+                # logic for adding a new poi that is not the first
+                else:
+                    prev = pois[-1]
+                    route_info = client.compute_route(prev['name'], poi_to_add, prev['departure'])
+                    
+                    if route_info is None:
+                        flash(message="There was an error in route computation, please try again")
+                    else:
+                        trip.pois.append({
+                                            'name': poi_to_add,
+                                            "departure": departure_datetime
+                                        })
+                        trip.routes.append({'route': route_info})
+                        pprint(route_info)
+                        trip.save()
+                
+                # reload
+                return redirect(url_for("trips.plan_trip", trip_title=trip.title))
     else:
         trip_info = []
     
-    if request.method == "POST":
-        
-        if form.validate_on_submit():
-            poi_to_add = form.poi.data
-            leave_poi_t = form.depart.data
-            depart_cp = trip.start_time
-            
-            # formatting time as datetime object
-            departure_datetime = datetime(depart_cp.year,
-                                          depart_cp.month,
-                                          depart_cp.day,
-                                          leave_poi_t.hour,
-                                          leave_poi_t.minute)
-            
-            # logic for if we need to compute route from college park (adding first POI)
-            if not pois:
-                route_info = client.compute_route("University of Maryland, College Park", 
-                                                  poi_to_add, 
-                                                  depart_cp)
-                
-                if route_info is None:
-                    flash(message="There was an error in route computation, please try again")
-                else:
-                    trip.pois.append({
-                                        'name': poi_to_add,
-                                        "departure": departure_datetime
-                                    })
-                    trip.routes.append({'route': route_info})
-                    pprint(route_info)
-                    trip.save()
-            
-            # logic for adding a new poi that is not the first
-            else:
-                prev = pois[-1]
-                route_info = client.compute_route(prev['name'], poi_to_add, prev['departure'])
-                
-                if route_info is None:
-                    flash(message="There was an error in route computation, please try again")
-                else:
-                    trip.pois.append({
-                                        'name': poi_to_add,
-                                        "departure": departure_datetime
-                                    })
-                    trip.routes.append({'route': route_info})
-                    pprint(route_info)
-                    trip.save()
-            
-            # reload
-            return redirect(url_for("trips.plan_trip", trip_title=trip.title))
         
     return render_template('trip_planning.html', form=form, info=trip_info)
 
